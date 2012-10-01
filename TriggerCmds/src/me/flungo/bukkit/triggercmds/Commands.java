1cb7cc11aded254e1aa6f449bfc9ff3720ec1043
Fabrizio Lungo <fab@lungo.co.uk>
2012-10-01 15:41:48 +0100

    Cleaned up command handler and made note for future improvements. Started to expand "/tcmds set" command.

--- TriggerCmds/src/me/flungo/bukkit/triggercmds/Commands.java
+++ TriggerCmds/src/me/flungo/bukkit/triggercmds/Commands.java
@@ -55,21 +55,22 @@ public class Commands implements CommandExecutor {
                 p.sendMessage(ChatColor.GOLD +"LeftClick" + ChatColor.WHITE + " - Reg Trigger Link ");
                 p.sendMessage(ChatColor.GOLD +"RightClick" + ChatColor.WHITE + " - Del Trigger Link");
                 return false;
-            } else if (arg[0].equalsIgnoreCase("list")) {
-                Methods = new Methods(plugin);
+            }
+            Methods = new Methods(plugin);
+            if (arg[0].equalsIgnoreCase("list")) {
                 Methods.iList(p);
                 return true;
             } else if (arg[0].equalsIgnoreCase("edit")) {
-                Methods = new Methods(plugin);
                 Methods.RegEditInteraction(p, arg[1].toLowerCase());
                 return true;
             } else if (arg[0].equalsIgnoreCase("set")) {
-                Methods = new Methods(plugin);
-				String GetCmds = Methods.GetCmds(p.getName(), arg);
-				Methods.RegCmd(p, GetCmds);
-				return true;
+                if (arg[1].equalsIgnoreCase("cmd")) {
+                    String GetCmds = Methods.GetCmds(p.getName(), arg);
+                    Methods.RegCmd(p, GetCmds);
+                    return true; 
+                }
+
 			} else if (arg[0].equalsIgnoreCase("add")) {
-                Methods = new Methods(plugin);
 				String AddCmds = Methods.GetCmds(p.getName(), arg);
 				Methods.AddCmd(p, AddCmds);
 				return true;
@@ -77,18 +78,13 @@ public class Commands implements CommandExecutor {
                 plugin.reload();
                 return true;
             } else if (arg[0].equalsIgnoreCase("del")) {
-                Methods = new Methods(plugin);
                 Methods.delTrigger(p);
                 return true;
-            } else if (arg[0].equalsIgnoreCase("help")) {
-                p.sendMessage(ChatColor.GOLD + "[ " + ChatColor.GREEN + "TriggerCmds Help Page" + ChatColor.GOLD + " ]");
-                p.sendMessage(ChatColor.GOLD +"LeftClick" + ChatColor.WHITE + " - Reg Trigger Link ");
-                p.sendMessage(ChatColor.GOLD +"RightClick" + ChatColor.WHITE + " - Del Trigger Link");
-                return false;
             }
-                p.sendMessage(ChatColor.GOLD + "[ " + ChatColor.GREEN + "TriggerCmds Help Page" + ChatColor.GOLD + " ]");
-                p.sendMessage(ChatColor.GOLD +"LeftClick" + ChatColor.WHITE + " - Reg Trigger Link ");
-                p.sendMessage(ChatColor.GOLD +"RightClick" + ChatColor.WHITE + " - Del Trigger Link");
+            //"help" command will return the same as a bad command:
+            p.sendMessage(ChatColor.GOLD + "[ " + ChatColor.GREEN + "TriggerCmds Help Page" + ChatColor.GOLD + " ]");
+            p.sendMessage(ChatColor.GOLD +"LeftClick" + ChatColor.WHITE + " - Reg Trigger Link ");
+            p.sendMessage(ChatColor.GOLD +"RightClick" + ChatColor.WHITE + " - Del Trigger Link");
             return false;
         } catch (Exception e) {
             p.sendMessage(ChatColor.RED + "Input error, please try again.");
